d1f79c0faebf9f8801aea15895e3471e198b10e7
Khursheed <ka.pixelbits@gmail.com>
2025-10-27 02:13:27 +0500

    private the password and token from public

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -63,6 +63,16 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.tokens;
+  delete userObject.password;
+
+  return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user.id.toString() }, 'mykey', {
